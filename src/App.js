e9cd2369288bc05590e80b74b77eca8f3268b192
RazaMunir824 <muninraza0047@gmail.com>
2021-03-20 00:00:12 +0500

    Forms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import logo from "./images/logo.png";
 import Homepage from "./components/Homepage/index";
+import Login from "./components/Forms/Login";
+import Registration from "./components/Forms/Registration";
+import Hos_Registration from "./components/Forms/Hos_Registration";
+import AddChild from "./components/Forms/AddChild";
 
 export default function App() {
-  return <Homepage logo={logo}></Homepage>;
+  return (
+  	    <div>
+          <Login />
+          <Registration />
+          <Hos_Registration />
+          <AddChild />
+        </div>
+    )
+         
 }
